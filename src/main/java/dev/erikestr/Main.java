89b9312c988a6ff89a1071e7db22162fc47a2c18
erikestr <erikestrada37@outlook.com>
2025-11-16 15:08:49 +0900

    feat: understanding Bean Programmatically Def

--- src/main/java/dev/erikestr/Main.java
+++ src/main/java/dev/erikestr/Main.java
@@ -1,5 +1,9 @@
 package dev.erikestr;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import dev.erikestr.beans.Vehicle;
@@ -8,10 +12,41 @@ import dev.erikestr.config.ProjectConfig;
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle ctxVehicle = context.getBean(Vehicle.class);
-        System.out.println("Vehicle from Spring Context using @PostConstruct: " + ctxVehicle.getName());
-        ctxVehicle.defaultVehicle();
-        System.out.println("Vehicle from Spring Context after defaultVehicle() call: " + ctxVehicle.getName());
+
+        Supplier<Vehicle> vehicleSupplier1 = () -> {
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName("default-1");
+            return vehicle;
+        };
+
+        Supplier<Vehicle> vehicleSupplier2 = () -> {
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName("default-2");
+            return vehicle;
+        };
+
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            context.registerBean("default1", Vehicle.class, vehicleSupplier1);
+        } else {
+            context.registerBean("default2", Vehicle.class, vehicleSupplier2);
+        }
+
+        try {   
+            Vehicle def1 = context.getBean("default1", Vehicle.class);
+            System.out.println("Vehicle using Bean Programmatic Registration: " + def1.getName());
+        }
+        catch (NoSuchBeanDefinitionException e) {
+            System.out.println("Bean 'default1' not found in context.");
+        }
+
+        try {   
+            Vehicle def2 = context.getBean("default2", Vehicle.class);
+            System.out.println("Vehicle using Bean Programmatic Registration: " + def2.getName());
+        }
+        catch (NoSuchBeanDefinitionException e) {
+            System.out.println("Bean 'default2' not found in context.");
+        }
 
         context.close();
     }
--- src/main/java/dev/erikestr/beans/Vehicle.java
+++ src/main/java/dev/erikestr/beans/Vehicle.java
@@ -1,21 +1,10 @@
 package dev.erikestr.beans;
 
-import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-
-@Component
 public class Vehicle {
     String name;
 
     public String getName() {
         return name;
-    }
-
-    @PostConstruct
-    public void init() {
-        this.name = "default-5";
     }
 
     public void setName(String name) {
@@ -25,11 +14,5 @@ public class Vehicle {
     public Vehicle defaultVehicle() {
         this.name = "default-4";
         return this;
-    }
-
-    @PreDestroy
-    public void destroy() {
-        this.name = null;
-        System.out.println("Vehicle bean is being destroyed.");
     }
 }
